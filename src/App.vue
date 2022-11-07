04f97ef81ae6d5eb11d5f3d83e352b6f36be6582
Nicolò Maria Semprini <nicosemp@gmail.com>
2022-11-07 07:37:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main></main>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
